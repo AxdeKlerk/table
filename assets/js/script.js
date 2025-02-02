8b6472da20014c0c03948e8908e7b236f65cf342
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-02-02 21:01:32 +0000

    chore: spelling

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,13 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
-    const queryString = `${API_KEY}?api_key=${API_KEY}`;
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
 
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data);
+        console.log(data.expiry);
     }
 }
